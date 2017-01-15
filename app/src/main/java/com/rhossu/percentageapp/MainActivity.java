9e56043b90e9951ef2283cef54744f7de0542b0d
roxanah <roxana_hossu@yahoo.ro>
2017-01-15 21:18:44 +0200

    Percentage App initial commit

---
+++ app/src/main/java/com/rhossu/percentageapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.rhossu.percentageapp;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvTotal;
+    EditText etPercentage, etNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tvTotal = (TextView) findViewById(R.id.tvTotal);
+        etPercentage = (EditText) findViewById(R.id.etPercentage);
+        etNumber = (EditText) findViewById(R.id.etNumber);
+
+        Button calculateButton = (Button) findViewById(R.id.btnCalculate);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float percentage = Float.parseFloat(etPercentage.getText().toString());
+                float dec = percentage / 100;
+                float total = dec * Float.parseFloat(etNumber.getText().toString());
+
+                tvTotal.setText(Float.toString(total));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
